919d6a172bcbb1946679f9e223b22ba820a57822
Christopher Turner <christopher.turner@mi9.com.au>
2015-09-02 10:16:49 +1000

    bumping package version for v0.1 release

--- index.js
+++ index.js
@@ -1,89 +1,99 @@
-module.exports = function isval(value, type) {
-  if (arguments.length === 2 && typeof type === 'undefined') {
-    type = 'undefined';
-  } else if (Number.isNaN(type)) {
-    type = 'NaN';
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['isval'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root['isval'] = factory();
   }
+})(this, function() {
+  return function isval(value, type) {
+    if (arguments.length === 2 && typeof type === 'undefined') {
+      type = 'undefined';
+    } else if (Number.isNaN(type)) {
+      type = 'NaN';
+    }
 
-  function isObject(obj) {
-    return typeof value === 'object' && value !== null;
-  }
+    function isObject(obj) {
+      return typeof value === 'object' && value !== null;
+    }
 
-  function constructorType(constructor) {
-    return constructor.name.toLowerCase();
-  }
+    function constructorType(constructor) {
+      return constructor.name.toLowerCase();
+    }
 
-  function isGenerator (obj) {
-    return obj &&
-      typeof obj.next === 'function' &&
-      typeof obj.throw === 'function'
-  }
+    function isGenerator(obj) {
+      return obj &&
+        typeof obj.next === 'function' &&
+        typeof obj.throw === 'function'
+    }
 
-  function isGeneratorFunction (fn) {
-    return typeof fn === 'function' &&
-      fn.constructor &&
-      fn.constructor.name === 'GeneratorFunction'
-  }
+    function isGeneratorFunction(fn) {
+      return typeof fn === 'function' &&
+        fn.constructor &&
+        fn.constructor.name === 'GeneratorFunction'
+    }
 
-  switch (type) {
-    case 'boolean':
-    case 'function':
-    case 'string':
-      return typeof value === type;
+    switch (type) {
+      case 'boolean':
+      case 'function':
+      case 'string':
+        return typeof value === type;
 
-    case 'number':
-      return typeof value === type && !isNaN(value);
+      case 'number':
+        return typeof value === type && !isNaN(value);
 
-    case Number:
-      if (isNaN(value)) { return false; }
-    // fall through
-    case String:
-    case Boolean:
-      return typeof value === constructorType(type);
+      case Number:
+        if (isNaN(value)) { return false; }
+      // fall through
+      case String:
+      case Boolean:
+        return typeof value === constructorType(type);
 
-    case Object:
-    case 'object':
-      return isObject(value);
+      case Object:
+      case 'object':
+        return isObject(value);
 
-    case 'array':
-      return Array.isArray(value);
+      case 'array':
+        return Array.isArray(value);
 
-    case 'regex':
-    case 'regexp':
-      return value instanceof RegExp;
+      case 'regex':
+      case 'regexp':
+        return value instanceof RegExp;
 
-    case 'date':
-      return value instanceof Date;
+      case 'date':
+        return value instanceof Date;
 
-    case 'null':
-    case null:
-      return value === null;
+      case 'null':
+      case null:
+        return value === null;
 
-    case 'undefined':
-      return typeof value === 'undefined';
+      case 'undefined':
+        return typeof value === 'undefined';
 
-    case 'NaN':
-      return Number.isNaN(value);
+      case 'NaN':
+        return Number.isNaN(value);
 
-    case 'arguments':
-      if (isObject(value)) {
-        return (typeof value.callee === 'function')
-          || (/arguments/i).test(value.toString());
-      }
-      return false;
+      case 'arguments':
+        if (isObject(value)) {
+          return (typeof value.callee === 'function')
+            || (/arguments/i).test(value.toString());
+        }
+        return false;
 
-    case 'integer':
-      return isval(value, 'number') && value % 1 === 0;
+      case 'integer':
+        return isval(value, 'number') && value % 1 === 0;
 
-    case 'generator':
-      return isGenerator(value);
+      case 'generator':
+        return isGenerator(value);
 
-    case 'generator*':
-      return isGeneratorFunction(value);
+      case 'generator*':
+        return isGeneratorFunction(value);
 
-    default:
-      return (typeof type === 'function')
-        ? value instanceof type
-        : value;
-  }
-};
+      default:
+        return (typeof type === 'function')
+          ? value instanceof type
+          : value;
+    }
+  };
+});
